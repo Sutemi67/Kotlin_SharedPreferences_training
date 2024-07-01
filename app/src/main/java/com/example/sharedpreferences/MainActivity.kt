887a81d38b93ee33389f6f91497696e6929bea28
sergeyboykov <sutemi67@gmail.com>
2024-07-01 10:10:03 +0300

    1

--- app/src/main/java/com/example/sharedpreferences/MainActivity.kt
+++ app/src/main/java/com/example/sharedpreferences/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.sharedpreferences
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
+const val PRACTICUM_EXAMPLE_PREFERENCES = "practicum_example_preferences"
+const val EDIT_TEXT_KEY = "key_for_edit_text"
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val editText = findViewById<EditText>(R.id.editText)
+        val saveButton = findViewById<Button>(R.id.saveButton)
+        val restoreButton = findViewById<Button>(R.id.restoreButton)
+
+        val sharedPref = getSharedPreferences(EDIT_TEXT_KEY, MODE_PRIVATE)
+
+        editText.setText(sharedPref.getString(EDIT_TEXT_KEY, ""))
+
+        saveButton.setOnClickListener {
+            sharedPref.edit()
+                .putString(EDIT_TEXT_KEY, editText.editableText.toString())
+                .apply()
+            Toast.makeText(this, "Saved ${editText.editableText}", Toast.LENGTH_SHORT).show()
         }
     }
 }
